acd0ca4d27250f253a8930b39a1255c46c1d0699
agrin <agrin.sadon@metropolia.fi>
2024-07-02 15:41:02 +0300

    Added Email, github , instagram and CV

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,8 @@
-// Home.js
 import { useState } from 'react';
 import Navbar from "../Components/Navbar.jsx";
 import '../Styles/Home.css';
 import fiTranslations from '../Translation/FIN.json';
+import { FaGithub, FaEnvelope, FaInstagram, FaFileAlt } from 'react-icons/fa';
 
 const Home = () => {
     const [translations, setTranslations] = useState(fiTranslations);
@@ -15,8 +15,21 @@ const Home = () => {
                 <h1>Agrin Sadon</h1>
                 <hr />
                 <h2>{translations.title}</h2>
+                <div className="icon-container">
+                    <a href="https://github.com/agrinsadon" target="_blank" rel="noopener noreferrer">
+                        <FaGithub className="icon"/>
+                    </a>
+                    <a href="https://www.instagram.com/agrin.sadon" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram className="icon"/>
+                    </a>
+                    <a href="mailto:sadon.code@gmail.com">
+                        <FaEnvelope className="icon"/>
+                    </a>
+                    <a href="/AgrinCV.pdf" target="_blank" rel="noopener noreferrer">
+                        <FaFileAlt className="icon"/>
+                    </a>
+                </div>
             </div>
-
         </div>
     );
 };
