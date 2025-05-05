03ef62119ba6449ab9f7ae48518ea75391d643d9
agrinsadon <sadon.code@gmail.com>
2025-05-05 19:52:22 +0300

    Added a palm tree component to the landing page and updated the data in the ENG and FIN translation files.

---
+++ src/Components/PalmTree.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { ReactP5Wrapper } from "react-p5-wrapper";
+import "../Pages/LandingPage/Home.css"; // Adjust the path as necessary
+
+const Sketch = (p5) => {
+  let baseLength = 120;
+  let angle;
+
+  p5.setup = () => {
+    p5.createCanvas(300, 300); // Smaller canvas
+    p5.angleMode(p5.RADIANS);
+    p5.strokeCap(p5.ROUND);
+  };
+  
+  p5.draw = () => {
+    p5.clear();
+  
+    p5.push();
+    const scaleFactor = 0.5;
+    p5.scale(scaleFactor);
+  
+    // Recenter the tree for the smaller canvas
+    p5.translate(150 / scaleFactor, 295 / scaleFactor);
+  
+    angle = p5.map(p5.sin(p5.frameCount * 0.01), -1, 1, p5.PI / 6, p5.PI / 7);
+    drawTree(baseLength, 12);
+    p5.pop();
+  };
+  
+  function drawTree(len, weight) {
+    p5.stroke("#8892b0");
+    p5.strokeWeight(weight);
+    p5.line(0, 0, 0, -len);
+    p5.translate(0, -len);
+
+    if (len > 10) {
+      p5.push();
+      p5.rotate(angle);
+      drawTree(len * 0.75, weight * 0.6);
+      p5.pop();
+
+      p5.push();
+      p5.rotate(-angle);
+      drawTree(len * 0.75, weight * 0.6);
+      p5.pop();
+    } else {
+      drawLeaf(p5);
+    }
+  }
+
+  function drawLeaf(p5) {
+    const size = 6;
+    p5.noStroke();
+    p5.fill("#64ffda");
+    p5.ellipse(0, 0, size, size);
+  }
+};
+
+const SamuraiTree = () => (
+  <div id="samurai-tree">
+    <ReactP5Wrapper sketch={Sketch} />
+  </div>
+);
+
+export default SamuraiTree;
--- src/Pages/LandingPage/Home.jsx
+++ src/Pages/LandingPage/Home.jsx
@@ -1,41 +1,43 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import Navbar from "../../Components/Navbar.jsx";
-import './Home.css';
 import fiTranslations from '../../Translation/FIN.json';
 import { FaGithub, FaEnvelope, FaLinkedin } from 'react-icons/fa';
+import PalmTree from '../../Components/PalmTree.jsx'; // No changes needed here
+import './Home.css';
 
-// eslint-disable-next-line react/prop-types
 const Home = ({ onTranslationsChange }) => {
-    const [translations, setTranslations] = useState(fiTranslations);
+  const [translations, setTranslations] = useState(fiTranslations);
 
-    const handleTranslationsChange = (newTranslations) => {
-        setTranslations(newTranslations);
-        onTranslationsChange(newTranslations);
-    };
+  const handleTranslationsChange = (newTranslations) => {
+    setTranslations(newTranslations);
+    onTranslationsChange(newTranslations);
+  };
 
-    return (
-        <>
-            <Navbar onTranslationsChange={handleTranslationsChange} />
-
-            <div className="LandingPage">
-                <h1>Agrin Sadon</h1>
-                <hr />
-                <h2>{translations.title}</h2>
-                <div className="home-icon-container">
-                    <a href="https://github.com/agrinsadon" target="_blank" rel="noopener noreferrer">
-                        <FaGithub className="home-icon"/>
-                    </a>
-                    <a href="https://www.linkedin.com/in/agrin-sadon-25724a265" target="_blank" rel="noopener noreferrer">
-                        <FaLinkedin className="home-icon"/>
-                    </a>
-                    <a href="mailto:sadon.code@gmail.com">
-                        <FaEnvelope className="home-icon"/>
-                    </a>
-                </div>
+  return (
+    <>
+      <Navbar onTranslationsChange={handleTranslationsChange} />
+      <div className="LandingPage">
+          <PalmTree />
+        <div className="home-title">  
+        <h1>{translations.title}</h1>
+        </div>
+        <div className="home-description">
+            <h2>{translations.subtitle}</h2>
             </div>
-        </>
-    );
+        <div className="home-icon-container">
+          <a href="https://github.com/agrinsadon" target="_blank" rel="noopener noreferrer">
+            <FaGithub className="home-icon"/>
+          </a>
+          <a href="https://www.linkedin.com/in/agrin-sadon-25724a265" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="home-icon"/>
+          </a>
+          <a href="mailto:sadon.code@gmail.com">
+            <FaEnvelope className="home-icon"/>
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
